f3b76bb3c7f71e23c16286ba564102a1befdade6
RHVDM <100280974+RHVDM@users.noreply.github.com>
2025-07-04 14:46:59 +0200

    Rhvdm/4 add read csv (#7)
    * Add argparse input and output as defaults
    
    * Add project file (#5) (#6)
    
    * Co-authored-by: Mahmoud Abdelrazek <razekmh@users.noreply.github.com>
    
    * Add initial python version
    
    * Updated python version from 3.10 to 3.12
    
    * Add initial pytoml
    
    * Fixed _name__ by correcting _main_  to __main__
    
    * Add input file converted to output with path
    
    * Add suffex to output file

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-# from utils import read_csv
+from utils import read_csv, write_csv
 import argparse
 import pathlib
 
 parser = argparse.ArgumentParser(
         description='Preproccess files to match expect card format in Arches')
 parser.add_argument(
-        '-in', "--input", metavar='str', type=str,
-        help='file to be preprocessed')
-# parser.add_argument(
-#         '--log', default=sys.stdout, type=argparse.FileType('w'),
-#         help='the file where the sum should be written')
+        '-iof', "--input_data_folder", metavar='str', type=str,
+        help='folder to be preprocessed', default='data')
+parser.add_argument( '-f', "--file", metavar='str', type=str,
+        help='file to be preprocessed', default='data.csv')
+parser.add_argument(
+        '-od', "--output_directory", metavar='str', type=str,
+        help='folder to be written to', default='output')
+
 args = parser.parse_args()
 
 
 def preprocess(args):
-    print(args.input)
+    input_data_folder = pathlib.Path(args.input_data_folder)
+    output_folder = pathlib.Path(args.output_directory)
+    data_file = pathlib.Path(args.file)
+    df = read_csv(input_data_folder / data_file)
+    output_file = data_file.stem + '_processed.csv'
+    write_csv(df, output_folder / output_file)
+    print(f"Input folder: {input_data_folder}")
+    print(f"{output_folder=}")
 
 
-
-
-
-
-if __name__ == "main":
+if __name__ == "__main__":
     preprocess(args)
